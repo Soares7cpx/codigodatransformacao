f812498dc160e09cd203ba7afa9fe09455e5536d
soarezzzk <henzosoaresgomes409@gmail.com>
2025-04-04 16:42:30 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+print("Olá Mundo")
+print("Corinthians sem tradição na liberta")
+print("Messi melhor que CR7")
+
+caixa= 'celular'
+print("caixa")
+
+nome= 'Henzo'
+idade= 15
+altura= 1,73
+72
+estudante= True
+print(nome, idade, altura, estudante)
+
+mensagem= 'TeLeViSaO'
+print(mensagem.strip())
+print(mensagem.upper())
+print(mensagem.lower())
+
+nome=input('Qual é o seu nome? ')
+print(f'Olá {nome}, Seja muito bem vindo ao codigo da transformação')
+from datetime import datetime
+nome= input('Qual é o seu nome?')
+agora = datetime.now()
+hora_atual = agora.strftime('%H:%M')
+print(f'{nome}! Agora são {hora_atual}')
